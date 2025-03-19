76ef421d7578c77a21cfce1f01823acf1bef826d
Christopher Jury <christopher.p.j.jury@gmail.com>
2025-03-19 21:11:24 +1100

    Snippet changes

--- lua/configs/luasnip.lua
+++ lua/configs/luasnip.lua
@@ -1,5 +1,12 @@
 
 print(vim.fn.stdpath("config"))
 print(vim.fn.stdpath("config") .. "/snips/")
-require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_vscode").lazy_load({paths = {vim.fn.stdpath("config") .. "/snips/"}})
+-- require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_lua").load({paths = {"~/.config/nvim/snips/luasnippets/"}})
+require("luasnip.loaders.from_vscode").lazy_load({paths = {vim.fn.stdpath("config") .. "/snips/vscode/"}})
+
+-- Somewhere in your Neovim startup, e.g. init.lua
+require("luasnip").config.set_config({ -- Setting LuaSnip config
+  -- Use <Tab> (or some other key if you prefer) to trigger visual selection
+  store_selection_keys = "<Tab>",
+})
---
+++ snips/luasnippets/cpp.lua
@@ -0,0 +1,50 @@
+-- Place this in ${HOME}/.config/nvim/LuaSnip/all.lua
+--
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+
+local get_include_guard_name = function(args, parent)
+  local name = vim.fn.expand("%:r")
+  name = name:gsub("%/","_"):gsub("include_",""):upper()
+  return sn(nil, i(1, name))
+end
+
+local get_visual = function(args, parent)
+  if (#parent.snippet.env.LS_SELECT_RAW > 0) then
+    return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
+  else  -- If LS_SELECT_RAW is empty, return a blank insert node
+    return sn(nil, i(1))
+  end
+end
+
+return {
+  s({trig = "tii", dscr = "Expands 'tii' into LaTeX's textit{} command."},
+    fmta("\\textit{<>}",
+      {
+        d(1, get_visual),
+      }
+    )
+  ),
+  s({trig = "guard", dscr= "Header include guard"},
+    fmta(
+      [[
+      #ifndef <>
+      #define <>
+      <>
+      #endif /* <> */
+      ]],
+      {
+        d(1, get_include_guard_name), rep(1), i(0), rep(1)
+      }
+    )
+  ),
+
+}
